4487c47d4f5984951c59026dffbd49713e0eac47
Joaquin Oria <joria@smata.com.ar>
2018-05-03 15:13:34 -0300

    Se agregaron mas test de calculos.

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -1,4 +1,5 @@
 package calculadora;
+
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptEngine;
 import javax.script.ScriptException;
@@ -7,35 +8,37 @@ public class Calculadora {
 
 	ScriptEngineManager mgr;
 	ScriptEngine engine;
-	
+
 	public Calculadora() {
 		mgr = new ScriptEngineManager();
 		engine = mgr.getEngineByName("JavaScript");
 	}
-	
-	
+
 	private String resolver(String expresion) throws ScriptException {
-		String result = engine.eval(expresion).toString();
-		//Saco el .0
-		return result.indexOf(".") < 0 ? result : result.replaceAll("0*$", "")
-				.replaceAll("\\.$", "");
+		String r = engine.eval(expresion).toString();
+
+		// Saco el .0
+		return r.indexOf(".") < 0 ? r : r.replaceAll("0*$", "").replaceAll("\\.$", "");
 	}
 
-
 	public String resolverExpresionMatematica(String expresion) throws ScriptException {
-		expresion = expresion.replaceAll(" ", "").replaceAll("(\\w+)\\^(\\w+)", "Math.pow($1,$2)");
+		// reemplazo ^ por Math.pow()
+		expresion = expresion.replaceAll(" ", "")
+				.replaceAll("(\\w+)\\^(\\w+)", "Math.pow($1,$2)");
+
 		String aux[] = expresion.split("%");
-		
-		if(aux.length > 1) {
-			String tanto = "(" + aux[0] + "/100) *";
-			
+
+		if (aux.length > 1) {
+			// Divido el porcentaje por 100 para después poder multiplicarlo por el total.
+			String tanto = "(" + aux[0] + "/100) * (";
+
 			String total = aux[1].replace("DE", "");
-			
-			expresion = resolver(tanto + total);
-						
+
+			expresion = resolver(tanto + total + ")");
+
 			return resolver(expresion);
 		}
-		
-	    return resolver(expresion);
+
+		return resolver(expresion);
 	}
 }
