ed3d9116b14c4c59920acdebc3ed49dd06431bc3
Oria Joaquin <joaquin.ariel.o@gmail.com>
2018-07-08 20:19:55 -0300

    Se agregaron mas campos a la entidad usuario.

--- src/modelos/Usuario.java
+++ src/modelos/Usuario.java
@@ -3,7 +3,10 @@ package modelos;
 public class Usuario {
 	private int id;
 	private String alias;
-	
+	private String nombre;
+	private String apellido;
+	private String email;
+	private String contrasenia;
 	public int getId() {
 		return id;
 	}
@@ -13,12 +16,37 @@ public class Usuario {
 	public String getAlias() {
 		return alias;
 	}
-	public void setAlias(String nombre) {
-		this.alias = nombre;
+	public void setAlias(String alias) {
+		this.alias = alias;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getContrasenia() {
+		return contrasenia;
+	}
+	public void setContrasenia(String contrasenia) {
+		this.contrasenia = contrasenia;
 	}
 	
 	@Override
 	public String toString() {
-		return "Usuario [id=" + id + ", alias=" + alias + "]";
+		return "Usuario [id=" + id + ", alias=" + alias + ", nombre=" + nombre + ", apellido=" + apellido + ", email="
+				+ email + ", contrasenia=" + contrasenia + "]";
 	}
 }
